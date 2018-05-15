4c1d7a359c2f6d4f0dfe7362ef5ed339516c41a2
Matt <mhooper72@gmail.com>
2018-05-15 15:23:38 -0600

    added backend.middlewares/auth and installed dotenv, set up auth register async functions

---
+++ backend/middlewares/auth.js
@@ -0,0 +1,38 @@
+const db = require('../models');
+const jwt = require('../jsonwebtoken');
+
+exports.register = async (req, res, next) => {
+  try {
+    //create a user
+    const user = await db.User.create(req.body);
+    // destructure req.body
+    const { id, email } = user.local;
+    //create a token (sign the token)
+    const token = jwt.sign(
+      {
+        id,
+        email
+      },
+      process.env.SECRET_KEY_LOCAL_AUTH
+    );
+    return res.staus(200).json({
+      id,
+      email,
+      token
+    });
+  } catch (err) {
+    // see what kind of error
+    // if validation error \/
+    if (err.code === 11000) {
+      // if occupied email send error
+      err.message = 'Sorry, that email is taken';
+    }
+    // otherwise, generic error
+    return next({
+      status: 400,
+      message: err.message
+    });
+  }
+};
+
+exports.signin = () => {};
