58086f3a76afbed578b13d3ea1da54ab6f2c16aa
Matt <mhooper72@gmail.com>
2018-05-17 12:40:21 -0600

    created authorization and authentication middleware

---
+++ backend/middlewares/authUser.js
@@ -0,0 +1,54 @@
+require('dotenv').load();
+const jwt = require('jsonwebtoken');
+
+// ********
+// jwt doesn't yet support async so using callbacks
+// ********
+
+// make sure use is logged in - Authentication
+exports.loginRequired = function(req, res, next) {
+  try {
+    const token = req.headers.authorization.spilt(' ')[1]; // Bearer_[token]
+    jwt.verify(token, process.env.SECRET_KEY, function(err, decodedPayload) {
+      if (decodedPayload) {
+        return next();
+      } else {
+        return next({
+          status: 401,
+          message: 'Please log in first'
+        });
+      }
+    });
+  } catch (err) {
+    return next({
+      status: 401,
+      message: 'Please log in first'
+    });
+  }
+};
+
+// /api/comments/:id
+// verify this http
+
+// make sure we get the correct user - Authorization
+exports.ensureCorrectUser = function(req, res, next) {
+  try {
+    const token = req.headers.authorization.split(' ')[1];
+    jwt.verify(token, process.env.SECRET_KEY, function(err, decodedPayload) {
+      // check if decodedPayload matches the id in the http post
+      if (decodedPayload && decodedPayload.id === req.params.id) {
+        return next();
+      } else {
+        return next({
+          status: 401,
+          massage: 'You are not authorized for this action'
+        });
+      }
+    });
+  } catch (err) {
+    return next({
+      status: 401,
+      message: 'Please log in first'
+    });
+  }
+};
